812b54429f53f32193a6709b1208f6b29d8e7fa2
pranavverma550 <pranavverma550@gmail.com>
2021-05-08 06:48:26 +0530

    ngModal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'modalng';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'ng-bootstrap-modal-demo';
+  closeResult: string;
+  modalOptions: NgbModalOptions;
+
+  constructor(
+    private modalService: NgbModal
+  ){
+    this.modalOptions = {
+      backdrop : 'static',
+      backdropClass : 'customBackdrop'
+    };
+  }
+  @ViewChild('myModal1') myModal1: ElementRef;
+  open(content): void {
+    this.modalService.open(content, this.modalOptions).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  ngAfterViewInit(): void{
+    this.myModal1.nativeElement.click();
+  }
 }
